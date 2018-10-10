97c28ccd4577287900e6f3b0a72a1bd4d4f5e8ec
dvmlive <40193000+dvmlive@users.noreply.github.com>
2018-10-10 13:03:21 -0400

    Added user story number in printing part

--- UserStory18.java
+++ UserStory18.java
@@ -21,19 +21,18 @@ public class UserStory18 {
             {
                 for(int i=0;i<childarray.length;i++)
                 {
-                    //System.out.println("Children are "+childarray[i]);
+
                     indivalues=indiMap.get(childarray[i].trim());
-                    //System.out.println(indivalues[7]);
+
                     spusearray=indivalues[7].split(" ");
-                    //System.out.println(spusearray.length);
-                    //System.out.println(spusearray[0]);
+
                     for(int j=0;j<spusearray.length;j++)
                     {
 
-                        //System.out.println("Hello"+spusearray[j]);
+
                         try {
                             familycomp = familyMap.get(spusearray[j]);
-                            // System.out.println(familycomp[0]+familycomp[4]);
+
                             if (indivalues[1].trim().equalsIgnoreCase("M")) {
                                 spouseName.put(childarray[i],familycomp[4]);
                             } else if (indivalues[1].trim().equalsIgnoreCase("F")) {
@@ -52,7 +51,7 @@ public class UserStory18 {
                     {
                         if(spouseName.get(z).equalsIgnoreCase(childarray[y]))
                         {
-                            System.out.println("Error in GEDCOM File:"+z+" "+spouseName.get(z)+" are couples and siblings\n");
+                            System.out.println("Error in GEDCOM File_User Story18 :"+z+" "+spouseName.get(z)+" are couples and siblings\n");
                         }
 
                     }
