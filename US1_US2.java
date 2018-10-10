9284e69713a14a578cbdb15543d87d3a650f5e82
dvmlive <40193000+dvmlive@users.noreply.github.com>
2018-10-10 12:52:46 -0400

    Added user story number in printing part

--- US1_US2.java
+++ US1_US2.java
@@ -26,13 +26,13 @@ public class US1_US2 {
                     birthDate = formatGEDCOM.StringtoDate(indiValues[2]);
 
                     if (birthDate.after(tDate)) {
-                        System.out.println("Error in GEDCOM File: Individual with individual ID " + s + " is having birth date after " +
+                        System.out.println("Error in GEDCOM File__UserStory1: Individual with individual ID " + s + " is having birth date after " +
                                 "today's date\n");
                     }
                 } else if (!(indiValues[2].equalsIgnoreCase("NA"))) {
                     deathDate = formatGEDCOM.StringtoDate(indiValues[4]);
                     if (deathDate.after(tDate)) {
-                        System.out.println("Error in GEDCOM File: Individual with individual ID " + s + " is having death date after " +
+                        System.out.println("Error in GEDCOM File_UserStory1: Individual with individual ID " + s + " is having death date after " +
                                 "today's date\n");
                     }
                 }
@@ -48,13 +48,13 @@ public class US1_US2 {
                 if (!(familyValues[0].trim().equalsIgnoreCase("NA"))) {
                     marriedDate = formatGEDCOM.StringtoDate(familyValues[0]);
                     if (marriedDate.after(tDate)) {
-                        System.out.println("Error in GEDCOM File: Family with Family ID " + t + " is having marriage date after " +
+                        System.out.println("Error in GEDCOM File_UserStory1: Family with Family ID " + t + " is having marriage date after " +
                                 "today's date\n");
                     }
                 } else if (!(familyValues[1].trim().equalsIgnoreCase("NA"))) {
                     divorceDate = formatGEDCOM.StringtoDate(familyValues[1]);
                     if (divorceDate.after(tDate)) {
-                        System.out.println("Error in GEDCOM File: Family with Family ID " + t + " is having divorce date after " +
+                        System.out.println("Error in GEDCOM File_UserStory1: Family with Family ID " + t + " is having divorce date after " +
                                 "today's date\n");
                     }
                 }
@@ -83,12 +83,12 @@ public class US1_US2 {
                 birthDateWife=formatGEDCOM.StringtoDate(indiValuesWife[2]);
                 if(birthDateHus.after(marriedDate))
                 {
-                    System.out.println("Error in GEDCOM File: Husband's birth date is after married date for the family " +
+                    System.out.println("Error in GEDCOM File_UserStory2: Husband's birth date is after married date for the family " +
                             "with family ID "+s+"\n");
                 }
                 if(birthDateWife.after(marriedDate))
                 {
-                    System.out.println("Error in GEDCOM File: Wife's birth date is after married date for the family " +
+                    System.out.println("Error in GEDCOM File_UserStory2: Wife's birth date is after married date for the family " +
                             "with family ID "+s+"\n");
                 }
 
