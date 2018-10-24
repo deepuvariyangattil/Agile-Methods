11861e77a0912668d6155acf0c1fb3238f614ac1
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-10-24 13:23:25 -0400

    Added Vedant's Test file for US22

---
+++ Test Files/UserStory22_TEST.java
@@ -0,0 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserStory22_TEST {
+
+    uniqueIndividualAndFamilyIDCheck obj = new uniqueIndividualAndFamilyIDCheck();
+
+
+    @Test
+    public void UniqueIDCheck_US22() {
+
+
+
+            Boolean b = obj.uniqueIDCheck("C:\\Users\\DEEPUAISHU\\IdeaProjects\\Sprint2\\Test\\Vedant.ged");
+            Assert.assertFalse(b);
+
+
+            Boolean c = obj.uniqueIDCheck("C:\\Users\\DEEPUAISHU\\IdeaProjects\\Sprint2\\Test\\Vedant(with error).ged");
+            Assert.assertTrue(c);
+
+
+
+    }
+}
+
+
+
+
+
