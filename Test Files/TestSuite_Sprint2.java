f33e92775392aa4c07d4c6b9e9378dbde830f63c
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-10-24 13:26:12 -0400

    Test suite for Sprint2

---
+++ Test Files/TestSuite_Sprint2.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MarriageAfter14AndSameLastNameCheckUS10US16Test.class,OrphanAndLargeAge_US33US34Test.class,PersonBeforeDeathScenarioTest.class,UserStory22_TEST.class,uniqueNameAndBirthdayCheckTest.class})
+
+public class TestSuite_Sprint2 {
+
+
+}
