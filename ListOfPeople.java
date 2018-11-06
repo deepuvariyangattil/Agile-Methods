5353924d92a610caa151c320137cfe29c8b61f57
Vedant Soni <vsoni7@stevens.edu>
2018-11-06 12:38:11 -0500

    Update ListOfPeople.java

--- ListOfPeople.java
+++ ListOfPeople.java
@@ -41,29 +41,5 @@ public class ListOfPeople {
         errorMessages.ListPrint("Living Married people", living_Married, "UserStory30");
         return result;
     }
-    
-     private FormatGEDCOM formatGEDCOM = new FormatGEDCOM();
-
-    public ListofPeople(){}
-
-    public boolean listDeceased_US29( HashMap<String, String[]> individual) throws ParseException {
-
-        formatGEDCOM.GedcomTable();
-        //HashMap<String, String[]> Individual = formatGEDCOM.getIndividualMap();
-        String[] indiValues = new String[100];
-        ErrorMessages em = new ErrorMessages();
-        boolean output = false;
-
-        for (String s : individual.keySet()) {
-            indiValues = individual.get(s);
-
-            if (indiValues[4] != "True" && indiValues[5] != "NA") {
-
-                em.IndividualTableErrorMessages("The individual is listed as deceased",s, "US29");
-
-            }output =true;
-
-        }return output;
-    }
 
 }
