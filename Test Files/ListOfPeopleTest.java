a7aaf2ef8f29c2cc6c3267efcc32c38a7bc3d3ee
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-06 13:11:21 -0500

    Added test case for Userstory 29

--- Test Files/ListOfPeopleTest.java
+++ Test Files/ListOfPeopleTest.java
@@ -37,4 +37,21 @@ public class ListOfPeopleTest {
 
 
     }
+	@Test
+    public void listDeceased__US29(){
+
+        ListofPeople obj = new ListofPeople();
+
+        HashMap<String,String[]> individual=new HashMap<>(100);
+
+        String[] indiValue1 = {"Christian/Philip/", "M", "16-11-1986", "25", "False", "08-02-2011", "F8"} ;
+        individual.put("I1", indiValue1);
+
+        try {
+            Assert.assertTrue(obj.listDeceased_US29(individual));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
