71684851224812e6c05474d860977b2aabfd7612
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-07 12:53:03 -0500

    Added after correcting errors

--- uniqueChildInFamily.java
+++ uniqueChildInFamily.java
@@ -1,47 +1,61 @@
 import java.util.HashMap;
+import java.util.Vector;
 
 public class uniqueChildInFamily {
 
     private FormatGEDCOM formatGEDCOM = new FormatGEDCOM();
 
-    public uniqueChildInFamily(){}
+    public uniqueChildInFamily() {
+    }
 
     public boolean uniqueChild_US25(HashMap<String, String[]> Family, HashMap<String, String[]> Individual) {
 
         formatGEDCOM.GedcomTable();
-        String[] famValues,childarray = new String[100];
+        String[] famValues, childarray = new String[100];
         String[] indiValues, indiValues1 = new String[100];
+        String[] childInfo = new String[100];
+        Vector<String> comparisonList = new Vector<>(100);
         ErrorMessages em = new ErrorMessages();
         boolean output = false;
 
         for (String s : Family.keySet()) {
             famValues = Family.get(s);
 
-            for (String s1 : Individual.keySet()) {
-                indiValues = Individual.get(s1);
 
-                        for(String s2 : Individual.keySet()){
-                            indiValues1 = Individual.get(s2);
+            childarray = famValues[6].trim().split(" ");
+            if (childarray.length > 1) {
 
-                            childarray=famValues[6].trim().split(" ");
-                            if(childarray.length > 1){
+                for (int i = 0; i < childarray.length; i++) {
+                    for (String indikey : Individual.keySet()) {
+                        if (indikey.equalsIgnoreCase(childarray[i])) {
+                            childInfo = Individual.get(indikey);
+                            comparisonList.add(indikey);
+                            comparisonList.add(childInfo[0]);
+                            comparisonList.add(childInfo[2]);
 
-                            for(int i=0; i<childarray.length; i++){
-
-                                if(s1.matches(childarray[i]) && s2.matches(childarray[i])){
-
-                                    if(indiValues[0] == indiValues1[0] && indiValues[2] == indiValues1[2]){
-
-                                    em.FamilyAndIndividualTableErrorMessages("Name and Birthdate same for children", s,s1,"US25");
-
-                                }output = true;
-                            }
                         }
                     }
                 }
 
+                for (int i = 1; i < comparisonList.size(); i = i + 2) {
+                    for (int j = i + 3; j < comparisonList.size(); j += 3) {
+                        
+                        if ((comparisonList.elementAt(i).equalsIgnoreCase(comparisonList.elementAt(j))) && (comparisonList.elementAt(i + 1).equalsIgnoreCase(comparisonList.elementAt(j + 1)))) {
+
+                            String IDs = comparisonList.elementAt(i - 1) + "," + comparisonList.elementAt(j - 1);
+                            em.IndividualTableErrorMessages("Name and Birthdate same for children", IDs, "UserStory25");
+                            output = true;
+                        }
+
+                    }
+
+                }
             }
-        }return output;
+            comparisonList.clear();
+        }
+
+
+        return output;
 
     }
 }
