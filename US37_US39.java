cea91c13fea3c770746b9a94e46e4c61e0bbccca
Nisheet Thakur <nisheet.thakur@gmail.com>
2018-11-07 12:28:54 -0500

    updated

--- US37_US39.java
+++ US37_US39.java
@@ -13,6 +13,7 @@ public class US37_US39 {
 	public static boolean recentSurvivors(HashMap<String,String[]> Indi) throws ParseException
 	{
 		boolean t=false;
+		ErrorMessages em = new ErrorMessages(); 
 		String[] indiValues=new String[100];
 		String[] indiValues2=new String[100];
 		String[] famValues=new String[100];
@@ -43,7 +44,7 @@ public class US37_US39 {
 			        		indiValues2=Indi.get(s1);
 			        		if((indiValues[7].equals(indiValues2[7])|| indiValues[7].equals(indiValues2[6])) &&indiValues2[4]=="True"&&!(indiValues[0].equals(indiValues2[0])))
 			        		{
-			        			System.out.println(indiValues2[0]);
+			        			em.ListPrint("Recent Survivors: ",indiValues2[0],"UserStory37");
 			        			t=true;
 			        		}
 			        		
@@ -60,41 +61,43 @@ return t;
 	{
 	boolean t=false;
 	String[] famValues=new String[100];
-	SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-    java.util.Date date1 = sdf.parse("01-11-2018");
-    java.util.Date date2 = sdf.parse("01-12-2018");
+	SimpleDateFormat sdf = new SimpleDateFormat("dd-MM");
+    int datestart=07;
+	int dateend=07;
+	int monthstart=11;
+    int monthend=12;
+    ErrorMessages em = new ErrorMessages();   
+
     System.out.println("User Story 39: Upcoming anniversaries:");
     for(String s:Fam.keySet())
     {
     	famValues=Fam.get(s);
     	 java.util.Date date = new Date(0);
-	        String formattedDate;
+	        String marriedmonth,marrieddate;
 	        try {
 				date = new SimpleDateFormat("dd-MM-yyyy").parse(famValues[0]);
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-	        formattedDate = dateFormat.format(date);
-	        if(date.after(date1)&&date.before(date2))
-	        {
-	        	System.out.println(famValues[3]+" and "+famValues[5]+" have an upcoming anniversary!");t=true;
-	        }
-    	
-    	
-    	
-    	
-    	
-    }
-	
-	
-	
-	return t;
-	
-		
-		
-	}
+	        DateFormat mo= new SimpleDateFormat("MM");
+			marriedmonth = mo.format(date);
+			int m=Integer.parseInt(marriedmonth);
+			DateFormat dt = new SimpleDateFormat("dd");
+			marrieddate=dt.format(date);
+			int d=Integer.parseInt(marrieddate);
+			
+			if(monthstart==m || monthend==m)
+			{
+				if((d>datestart && d<31) ||( d<dateend && d>0))
+				{
+				em.ListPrint("Upcoming anniversaries: ",famValues[3],"UserStory39");t=true;	
+				}
+				
+			}
+}
+return t;
+}
 	
 	
 	
