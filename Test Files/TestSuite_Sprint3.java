f306caad82976d2c665ebcd79a75e782774d6197
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-07 13:06:01 -0500

    Adding Test suite for sprint 2 and sprint 3

---
+++ Test Files/TestSuite_Sprint3.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestSuite_Sprint2.class,ListOfPeopleTest.class,GenderCorrectness_US21Test.class,uniqueChildInFamily_US25Test.class,DateComparisonTest.class})
+public class TestSuite_Sprint3 {
+
+}
