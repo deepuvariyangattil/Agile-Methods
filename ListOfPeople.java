ae81a518f2a86ab83d3729d0e9099623252e1349
Vedant Soni <vsoni7@stevens.edu>
2018-11-19 16:04:47 -0500

    Updated Us 35 & 36 with final changes.

--- ListOfPeople.java
+++ ListOfPeople.java
@@ -1,6 +1,6 @@
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Vector;
@@ -9,8 +9,9 @@ public class ListOfPeople {
 
     private FormatGEDCOM formatGEDCOM = new FormatGEDCOM();
 
-    public ListOfPeople(){}
-    
+    public ListOfPeople() {
+    }
+
     public boolean recentBirths_US35(HashMap<String, String[]> Individual) throws ParseException {
 
         formatGEDCOM.GedcomTable();
@@ -21,43 +22,38 @@ public class ListOfPeople {
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.MONTH, -1);
         Date checkDate = cal.getTime();
-        boolean output = false;
+        boolean output = true;
 
         for (String s : Individual.keySet()) {
             indiValues = Individual.get(s);
-            if (indiValues[4] == "True") {
 
-                java.util.Date date = new Date(0);
-                String fDate;
-                DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                fDate = dateFormat.format(date);
-                date = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[2]);
+            for (String s1 : Individual.keySet()) {
+                indiValues1 = Individual.get(s1);
 
-                try {
+                if (indiValues[4] == "True" && indiValues1[4] == "True") {
 
-                    if (date.after(checkDate)) {
-                        for (String s1 : Individual.keySet()) {
-                            indiValues1 = Individual.get(s1);
-                            {
+                    Date date = new Date();
+                    date = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[2]);
 
-                                if (indiValues[2].equals(indiValues1[2])) {
-                                    recent_Births.add(indiValues[0]);
-                                    output = true;
-                                }
+                    try {
+                        if (date.after(checkDate)) {
+
+                            if (indiValues[2].equals(indiValues1[2])) {
+                                recent_Births.add(indiValues[0]);
+                                output = false;
+
                             }
                         }
-                    }
 
-                } catch (Exception e) {
-                    continue;
+                    } catch (Exception e) {
+                        continue;
+                    }
                 }
             }
-        }
-        em.ListPrint("All Recent Births", recent_Births, "UserStory35" );
-        output = true;
+        }em.ListPrint("All Recent Births", recent_Births, "UserStory35" );
         return output;
     }
-
+    
     public boolean recentDeaths_US36(HashMap<String, String[]> Individual) throws ParseException {
 
         formatGEDCOM.GedcomTable();
@@ -68,40 +64,35 @@ public class ListOfPeople {
         Calendar cal = Calendar.getInstance();
         cal.add(Calendar.MONTH, -1);
         Date checkDate = cal.getTime();
-        boolean output = false;
+        boolean output = true;
 
         for (String s : Individual.keySet()) {
             indiValues = Individual.get(s);
-            if (indiValues[4] == "False") {
 
-                java.util.Date date = new Date(0);
-                String fDate;
-                DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-                fDate = dateFormat.format(date);
-                date = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[5]);
+            for (String s1 : Individual.keySet()) {
+                indiValues1 = Individual.get(s1);
 
-                try {
+                if (indiValues[4] == "False" && indiValues1[4] == "False") {
 
-                    if (date.after(checkDate)) {
-                        for (String s1 : Individual.keySet()) {
-                            indiValues1 = Individual.get(s1);
-                            {
+                    Date date = new Date();
+                    date = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[5]);
 
-                                if (indiValues[5].equals(indiValues1[5])) {
-                                    recent_Deaths.add(indiValues[0]);
-                                    output = true;
-                                }
+                    try {
+                        if (date.after(checkDate)) {
+
+                            if (indiValues[5].equals(indiValues1[5])) {
+                                recent_Deaths.add(indiValues[0]);
+                                output = false;
+
                             }
                         }
-                    }
 
-                } catch (Exception e) {
-                    continue;
+                    } catch (Exception e) {
+                        continue;
+                    }
                 }
             }
-        }
-        em.ListPrint("All Recent Deaths", recent_Deaths, "UserStory36" );
-        output = true;
+        }em.ListPrint("All Recent Deaths", recent_Deaths, "UserStory36"  );
         return output;
     }
 }
