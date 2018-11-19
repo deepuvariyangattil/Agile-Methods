c370a1238f633ff17caeca0ce9738f32d622ccf0
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-19 11:33:52 -0500

    After integrating US40

--- Sprint3.java
+++ Sprint3.java
@@ -1,30 +1,23 @@
 import java.text.ParseException;
 
 public class Sprint3 {
-    public static void main(String[] args) throws ParseException {
-        Sprint2 sprint2=new Sprint2();
+    public void Sprint3() throws ParseException {
+        Sprint2 sprint2 = new Sprint2();
         sprint2.Sprint2();
-        GenderCorrectness_US21 deepu_us21=new GenderCorrectness_US21();
-        ListOfPeople listOfPeople=new ListOfPeople();
-        uniqueChildInFamily vedant=new uniqueChildInFamily();
-        DateComparison susheel=new DateComparison();
-        FormatGEDCOM formatGEDCOM=new FormatGEDCOM();
+        GenderCorrectness_US21 deepu_us21 = new GenderCorrectness_US21();
+        ListOfPeople listOfPeople = new ListOfPeople();
+        uniqueChildInFamily vedant = new uniqueChildInFamily();
+        DateComparison susheel = new DateComparison();
+        FormatGEDCOM formatGEDCOM = new FormatGEDCOM();
         formatGEDCOM.GedcomTable();
-        System.out.println("\033[0;1mSprint 3 User stories");
-        System.out.println("\nDeepu's sprint 3 user stories\n");
-        deepu_us21.CorrectGender_US21(formatGEDCOM.getFamilyMap(),formatGEDCOM.getIndividualMap());
-        listOfPeople.ListLiving_US30(formatGEDCOM.getFamilyMap(),formatGEDCOM.getIndividualMap());
-        System.out.println("\nVedant's sprint 3 user stories\n");
-        listOfPeople.listDeceased_US29(formatGEDCOM.getIndividualMap());
-        vedant.uniqueChild_US25(formatGEDCOM.getFamilyMap(),formatGEDCOM.getIndividualMap());
-        System.out.println("\nNisheet's sprint 3 user stories\n");
-        listOfPeople.recentSurvivors_US37(formatGEDCOM.getIndividualMap());
-        listOfPeople.UpAnniv_39(formatGEDCOM.getFamilyMap());
-        System.out.println("\nSusheel's sprint 3 user stories\n");
-        susheel.BirthOfParentsWithChildBirthScenario_US08(formatGEDCOM.getIndividualMap(),formatGEDCOM.getFamilyMap());
-        susheel.DeathOfParentsWithChildBirthScenario_US09(formatGEDCOM.getIndividualMap(),formatGEDCOM.getFamilyMap());
+        deepu_us21.CorrectGender_US21(formatGEDCOM.getFamilyMap(), formatGEDCOM.getIndividualMap());
 
+        vedant.uniqueChild_US25(formatGEDCOM.getFamilyMap(), formatGEDCOM.getIndividualMap());
 
+        susheel.BirthOfParentsWithChildBirthScenario_US08(formatGEDCOM.getIndividualMap(), formatGEDCOM.getFamilyMap());
+        susheel.DeathOfParentsWithChildBirthScenario_US09(formatGEDCOM.getIndividualMap(), formatGEDCOM.getFamilyMap());
 
     }
+
+
 }
