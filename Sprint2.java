25ff157be0aa5b7482f35e76fcf0c24200486351
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-19 11:32:42 -0500

    After integrating US40

--- Sprint2.java
+++ Sprint2.java
@@ -7,17 +7,17 @@ public class Sprint2 {
     public void Sprint2() throws ParseException {
         Sprint1 sprint1=new Sprint1();
         MarriageAfter14AndSameLastNameCheck_US10US16 deepu_sprint2=new MarriageAfter14AndSameLastNameCheck_US10US16();
-        FormatGEDCOM formatGEDCOM=new FormatGEDCOM();
-        formatGEDCOM.GedcomTable();
+       FormatGEDCOM formatGEDCOM=new FormatGEDCOM();
+       formatGEDCOM.GedcomTable();
 
-        sprint1.sprint1();
+       sprint1.sprint1();
 
         deepu_sprint2.MarriageAfterFourteen_US10(formatGEDCOM.getFamilyMap(),formatGEDCOM.getIndividualMap());
-        deepu_sprint2.MaleLastNamesSameInFamily_US16(formatGEDCOM.getFamilyMap(),formatGEDCOM.getIndividualMap());
+       deepu_sprint2.MaleLastNamesSameInFamily_US16(formatGEDCOM.getFamilyMap(),formatGEDCOM.getIndividualMap());
 
-        uniqueIndividualAndFamilyIDCheck vedant_sprint2_US22=new uniqueIndividualAndFamilyIDCheck();
+       uniqueIndividualAndFamilyIDCheck vedant_sprint2_US22=new uniqueIndividualAndFamilyIDCheck();
         vedant_sprint2_US22.uniqueIDCheck("C:\\Users\\DEEPUAISHU\\IdeaProjects\\Sprint2\\src\\Deepu_Variyangattil.ged");
-        uniqueNameAndBirthdayCheck vedant_sprint2_US23=new uniqueNameAndBirthdayCheck();
+       uniqueNameAndBirthdayCheck vedant_sprint2_US23=new uniqueNameAndBirthdayCheck();
         vedant_sprint2_US23.uniqueNameandBirthday_US23(formatGEDCOM.getIndividualMap());
 
         PersonBeforeDeathScenario susheel_sprint2=new PersonBeforeDeathScenario();
@@ -25,7 +25,7 @@ public class Sprint2 {
         susheel_sprint2.checkMarriageBeforeDivorse(formatGEDCOM.getIndividualMap(),formatGEDCOM.getFamilyMap());
 
         OrphanAndLargeAge_US33US34 nisheet_Sprint2=new OrphanAndLargeAge_US33US34();
-        nisheet_Sprint2.orphans(formatGEDCOM.getIndividualMap(),formatGEDCOM.getFamilyMap());
+       nisheet_Sprint2.orphans(formatGEDCOM.getIndividualMap(),formatGEDCOM.getFamilyMap());
         nisheet_Sprint2.largeAgeD(formatGEDCOM.getIndividualMap());
 
     }
