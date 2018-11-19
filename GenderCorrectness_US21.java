acc03a3522283a6db1f5ed17b9d322b45a7119f4
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-19 11:25:37 -0500

    After integrating US40

--- GenderCorrectness_US21.java
+++ GenderCorrectness_US21.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class GenderCorrectness_US21 {
 
-
+IssueLine issueLine=new IssueLine();
     public boolean CorrectGender_US21(HashMap<String, String[]> Family, HashMap<String, String[]> Individual) {
         String[] familyValues, individualValues = new String[100];
         boolean result = false;
@@ -16,14 +16,16 @@ public class GenderCorrectness_US21 {
 
                         if (!(individualValues[1].trim().equalsIgnoreCase("M"))) {
                             String message = "Husband's gender is not Male";
-                            errorMessages.FamilyAndIndividualTableErrorMessages(message, familyValues[2], s, "UserStory21");
+                            int errorline=issueLine.GetLineNumber_Individual(t,individualValues[1]);
+                            errorMessages.FamilyAndIndividualTableErrorMessages(message, familyValues[2], s, "UserStory21",errorline);
                             result = true;
                         }
                     }
                 }
 
             } catch (Exception e) {
-                errorMessages.IndividualTableErrorMessages("Individual not present ", familyValues[2], "UserStory21");
+                int errorline=issueLine.GetLineNumber_Family(s,familyValues[2]);
+                errorMessages.IndividualTableErrorMessages("Individual not present ", familyValues[2], "UserStory21",errorline);
                 continue;
             }
             try {
@@ -32,13 +34,15 @@ public class GenderCorrectness_US21 {
                         individualValues = Individual.get(t);
                         if (!(individualValues[1].trim().equalsIgnoreCase("F"))) {
                             String message = "Wife's gender is not Male";
-                            errorMessages.FamilyAndIndividualTableErrorMessages(message, familyValues[4], s, "UserStory21");
+                            int errorline=issueLine.GetLineNumber_Individual(t,individualValues[1]);
+                            errorMessages.FamilyAndIndividualTableErrorMessages(message, familyValues[4], s, "UserStory21",errorline);
                             result = true;
                         }
                     }
                 }
             } catch (Exception e) {
-                errorMessages.IndividualTableErrorMessages("Individual not present ", familyValues[4], "UserStory21");
+                int errorline=issueLine.GetLineNumber_Family(s,familyValues[4]);
+                errorMessages.IndividualTableErrorMessages("Individual not present ", familyValues[4], "UserStory21",errorline);
                 continue;
             }
         }
