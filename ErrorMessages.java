73eb13f1d75fd52e2d6f7226db89b7c458640ee5
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-19 11:24:36 -0500

    After integrating US40

--- ErrorMessages.java
+++ ErrorMessages.java
@@ -1,22 +1,22 @@
 import java.util.Vector;
 
 public class ErrorMessages {
-    public boolean FamilyTableErrorMessages(String message,String familyID,String userStory)
+    public boolean FamilyTableErrorMessages(String message,String familyID,String userStory,int linenumber)
     {
         boolean result=true;
-        System.out.println("Error in GEDCOM File_"+userStory+" :"+message+" in the the family table with family ID "+familyID+"\n");
+        System.out.println("Error in GEDCOM File_"+userStory+"_"+linenumber+" :"+message+" in the family table with family ID "+familyID+"\n");
         return result;
     }
-    public boolean IndividualTableErrorMessages(String message,String individualID,String userStory)
+    public boolean IndividualTableErrorMessages(String message,String individualID,String userStory,int linenumber)
     {
         boolean result=true;
-        System.out.println("Error in GEDCOM File_"+userStory+" :"+message+" in the individual table with individual ID "+individualID+"\n");
+        System.out.println("Error in GEDCOM File_"+userStory+"_"+linenumber+" :"+message+" in the individual table with individual ID "+individualID+"\n");
         return result;
     }
-    public boolean FamilyAndIndividualTableErrorMessages(String message,String individualID,String familyID,String userStory)
+    public boolean FamilyAndIndividualTableErrorMessages(String message,String individualID,String familyID,String userStory,int linenumber)
     {
         boolean result=true;
-        System.out.println("Error in GEDCOM File_"+userStory+" :"+message+" in the family and individual tables with family ID and individual ID as "
+        System.out.println("Error in GEDCOM File_"+userStory+"_"+linenumber+" :"+message+" in the family and individual tables with family ID and individual ID as "
         +familyID+","+individualID+" respectively\n");
         return result;
     }
