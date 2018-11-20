92fc941c33d08b7d8425ffa8c331091c6c0d15da
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-20 13:57:44 -0500

    Test cases for complete project

---
+++ Test Files/TestSuite_Project.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestSuite_Sprint3.class,IssueLineTest.class,ParentsAgeAndSiblingsTest.class,US11_US14Test.class})
+
+public class TestSuite_Project {
+}
