1701ca82e2beaef818adb26f74c6d9eed030c3bd
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-20 13:46:31 -0500

    Added User story 35 &36

--- ListOfPeople.java
+++ ListOfPeople.java
@@ -5,6 +5,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Vector;
+import java.util.concurrent.TimeUnit;
 
 public class ListOfPeople {
     IssueLine issueLine=new IssueLine();
@@ -263,6 +264,77 @@ public class ListOfPeople {
         errorMessages.ListPrint("all multiple births in a GEDCOM file", list, "UserStory32");
         return result;
 
+    }
+    public boolean recentBirths_US35(HashMap<String, String[]> Individual) throws ParseException {
+
+
+        ErrorMessages em = new ErrorMessages();
+        String[] indiValues = new String[100];
+        Vector<String> recent_Births = new Vector<String>(100);
+        boolean output = false;
+
+        for (String s : Individual.keySet()) {
+            indiValues = Individual.get(s);
+
+
+            if (indiValues[4] == "True") {
+
+                Date date = new Date();
+                Date date1 = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[2]);
+
+                long duration = date.getTime() - date1.getTime();
+                long inDays = TimeUnit.MILLISECONDS.toDays(duration);
+
+                try {
+                    if (inDays < 30 && inDays>=0) {
+
+                        recent_Births.add(indiValues[0]);
+                        output = true;
+
+                    }
+
+                } catch (Exception e) {
+                    continue;
+                }
+            }
+        }em.ListPrint("Recent Births", recent_Births, "UserStory35" );
+        return output;
+    }
+
+    public boolean recentDeaths_US36(HashMap<String, String[]> Individual) throws ParseException {
+
+
+        ErrorMessages em = new ErrorMessages();
+        String[] indiValues = new String[100];
+        Vector<String> recent_Deaths = new Vector<String>(100);
+        boolean output = false;
+
+        for (String s : Individual.keySet()) {
+            indiValues = Individual.get(s);
+
+
+            if (indiValues[4] == "False") {
+
+                Date date = new Date();
+                Date date1 = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[5]);
+
+                long duration = date.getTime() - date1.getTime();
+                long inDays = TimeUnit.MILLISECONDS.toDays(duration);
+
+                try {
+                    if (inDays < 30&& inDays>=0) {
+
+                        recent_Deaths.add(indiValues[0]);
+                        output = true;
+
+                    }
+
+                } catch (Exception e) {
+                    continue;
+                }
+            }
+        }em.ListPrint("Recent Deaths", recent_Deaths, "UserStory36" );
+        return output;
     }
 
 }
