faac034fc0b49a4b218636959cc2394abf4120d6
Vedant Soni <vsoni7@stevens.edu>
2018-11-19 23:21:18 -0500

    Got the logic right for US35 and US36

--- ListOfPeople.java
+++ ListOfPeople.java
@@ -17,37 +17,31 @@ public class ListOfPeople {
         formatGEDCOM.GedcomTable();
         ErrorMessages em = new ErrorMessages();
         String[] indiValues = new String[100];
-        String[] indiValues1 = new String[100];
         Vector<String> recent_Births = new Vector<String>(100);
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.MONTH, -1);
-        Date checkDate = cal.getTime();
-        boolean output = true;
+        boolean output = false;
 
         for (String s : Individual.keySet()) {
             indiValues = Individual.get(s);
 
-            for (String s1 : Individual.keySet()) {
-                indiValues1 = Individual.get(s1);
 
-                if (indiValues[4] == "True" && indiValues1[4] == "True") {
+            if (indiValues[4] == "True") {
 
-                    Date date = new Date();
-                    date = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[2]);
+                Date date = new Date();
+                Date date1 = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[2]);
 
-                    try {
-                        if (date.after(checkDate)) {
+                long duration = date.getTime() - date1.getTime();
+                long inDays = TimeUnit.MILLISECONDS.toDays(duration);
 
-                            if (indiValues[2].equals(indiValues1[2])) {
-                                recent_Births.add(indiValues[0]);
-                                output = false;
+                try {
+                    if (inDays < 30) {
 
-                            }
-                        }
+                        recent_Births.add(indiValues[0]);
+                        output = true;
 
-                    } catch (Exception e) {
-                        continue;
                     }
+
+                } catch (Exception e) {
+                    continue;
                 }
             }
         }em.ListPrint("All Recent Births", recent_Births, "UserStory35" );
@@ -59,40 +53,34 @@ public class ListOfPeople {
         formatGEDCOM.GedcomTable();
         ErrorMessages em = new ErrorMessages();
         String[] indiValues = new String[100];
-        String[] indiValues1 = new String[100];
         Vector<String> recent_Deaths = new Vector<String>(100);
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.MONTH, -1);
-        Date checkDate = cal.getTime();
-        boolean output = true;
+        boolean output = false;
 
         for (String s : Individual.keySet()) {
             indiValues = Individual.get(s);
 
-            for (String s1 : Individual.keySet()) {
-                indiValues1 = Individual.get(s1);
 
-                if (indiValues[4] == "False" && indiValues1[4] == "False") {
+            if (indiValues[4] == "False") {
 
-                    Date date = new Date();
-                    date = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[5]);
+                Date date = new Date();
+                Date date1 = new SimpleDateFormat("dd-MM-yyyy").parse(indiValues[5]);
 
-                    try {
-                        if (date.after(checkDate)) {
+                long duration = date.getTime() - date1.getTime();
+                long inDays = TimeUnit.MILLISECONDS.toDays(duration);
 
-                            if (indiValues[5].equals(indiValues1[5])) {
-                                recent_Deaths.add(indiValues[0]);
-                                output = false;
+                try {
+                    if (inDays < 30) {
 
-                            }
-                        }
+                        recent_Deaths.add(indiValues[0]);
+                        output = true;
 
-                    } catch (Exception e) {
-                        continue;
                     }
+
+                } catch (Exception e) {
+                    continue;
                 }
             }
-        }em.ListPrint("All Recent Deaths", recent_Deaths, "UserStory36"  );
+        }em.ListPrint("All Recent Death", recent_Deaths, "UserStory36" );
         return output;
     }
 }
