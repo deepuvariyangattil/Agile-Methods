ef48a7973b92c82ea6d3974f46e40df1bc2cc50c
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-20 13:48:54 -0500

    Full project Main file

--- Project.java
+++ Project.java
@@ -11,12 +11,18 @@ public class Project {
         Sprint3 sprint3=new Sprint3();
         sprint3.Sprint3();
         US11_US14 us11_us14=new US11_US14();
+        ParentsAgeAndSiblings parentsAgeAndSiblings=new ParentsAgeAndSiblings();
         us11_us14.multipleBirths(formatGEDCOM.getIndividualMap());
         us11_us14.noBigamy(formatGEDCOM.getFamilyMap());
+        parentsAgeAndSiblings.checkParentsAgeDifference(formatGEDCOM.getIndividualMap(),formatGEDCOM.getFamilyMap());
+        parentsAgeAndSiblings.LessSiblings(formatGEDCOM.getFamilyMap());
+
 
         //List Printing
         System.out.println("\n\033[0;1m\033[0;4mDIFFERENT LISTS FROM GEDCOM FILE\033[0;0m\n");
         listOfPeople.UpcomingBday_US38(formatGEDCOM.getIndividualMap());
+        listOfPeople.recentBirths_US35(formatGEDCOM.getIndividualMap());
+        listOfPeople.recentDeaths_US36(formatGEDCOM.getIndividualMap());
         listOfPeople.ListLiving_US30(formatGEDCOM.getFamilyMap(), formatGEDCOM.getIndividualMap());
         listOfPeople.listDeceased_US29(formatGEDCOM.getIndividualMap());
         listOfPeople.recentSurvivors_US37(formatGEDCOM.getIndividualMap());
