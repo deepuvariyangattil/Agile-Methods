fcc15b5844c7d7907efda8fa2166ca42c443edbe
deepuvariyangattil <40193000+dvmlive@users.noreply.github.com>
2018-11-20 13:58:30 -0500

    Test cases for sprint1

---
+++ Test Files/TestSuite_Sprint1.java
@@ -0,0 +1,7 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({US1_US2Test.class,UserStory4UserStory5Test.class,UserStory07Test.class,UserStory18Test.class,ListOfPeopleTest.class})
+public class TestSuite_Sprint1 {
+}
